8abfe358714bf923362c424cca844d0eb3454695
Arseniy Shkunkov <Shkunkov.a@yandex.ru>
2023-05-07 23:03:35 +0200

    Fundamentals (#1)
    Task 0 implementation. Details: https://minitorch.github.io/module0/module0/

--- minitorch/module.py
+++ minitorch/module.py
@@ -31,13 +31,15 @@ class Module:
 
     def train(self) -> None:
         "Set the mode of this module and all descendent modules to `train`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        self.training = True
+        for module in self._modules.values():
+            module.train()
 
     def eval(self) -> None:
         "Set the mode of this module and all descendent modules to `eval`."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        self.training = False
+        for module in self._modules.values():
+            module.eval()
 
     def named_parameters(self) -> Sequence[Tuple[str, Parameter]]:
         """
@@ -47,13 +49,18 @@ class Module:
         Returns:
             The name and `Parameter` of each ancestor parameter.
         """
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        ls = list(self._parameters.items())
+        for module_name, module in self._modules.items():
+            for param_name, value in module.named_parameters():
+                ls += [(module_name + "." + param_name, value)]
+        return ls
 
     def parameters(self) -> Sequence[Parameter]:
         "Enumerate over all the parameters of this module and its descendents."
-        # TODO: Implement for Task 0.4.
-        raise NotImplementedError('Need to implement for Task 0.4')
+        ls = list(self._parameters.values())
+        for module in self._modules.values():
+            ls += module.parameters()
+        return ls
 
     def add_parameter(self, k: str, v: Any) -> Parameter:
         """
